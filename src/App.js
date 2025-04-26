86dcd332f30d04fc3262ab9be67421c9cbf41c01
kat-mart <katmartinez@chapman.edu>
2025-04-26 00:18:09 -0700

    add budgenator

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Navbar from './components/navbar'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+ const [data, setData] = useState(null);
+ const [response, setResponse] = useState('');
+
+
+ useEffect(() => {
+   axios.get('http://127.0.0.1:5000/api/user') // API endpoint from Flask
+     .then(response => {
+       setData(response.data);
+     })
+     .catch(error => {
+       console.error('There was an error!', error);
+     });
+ }, []);
+
+
+ const sendMessage = () => {
+   axios.post('http://127.0.0.1:5000/api/message', { message: "button clicked" })
+     .then(res => {
+       setResponse(res.data.response);
+     })
+     .catch(error => {
+       console.error('Error sending message:', error);
+     });
+ };
+
+
+ return (
+   <div>
+     {data ? <p>{data.name} {data.age}</p> : <p>Loading...</p>}
+     <button onClick={sendMessage}>Send Message</button>
+     {response && <p>{response}</p>}
+   </div>
+ );
 }
+
 
 export default App;

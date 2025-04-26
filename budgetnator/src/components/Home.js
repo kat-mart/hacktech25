e3d134371f1f86242516e7edfe7bfa292e654ef5
Kelly Ung <kellyung@myyahoo.com>
2025-04-26 12:05:32 -0700

    update Home.js

--- budgetnator/src/components/Home.js
+++ budgetnator/src/components/Home.js
@@ -1,11 +1,46 @@
 import './Home.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 export default function Home() {
+    const [response, setResponse] = useState('');
+    const [income, setIncome] = useState(null);
+    const [amount, setAmount] = useState(null);
+
+    const sendUserInput = () => { 
+        axios.post('http://127.0.0.1:5000/api/user-input', { Income: income })
+            .then(res => {
+            setResponse(res.data.response);
+            })
+            .catch(error => {
+            console.error('Error sending message:', error);
+            });
+    };
+
     return (
         <div>
-            <h1 className="title">BUGDETNATOR</h1>
-            <h2 className="title-description">Help you budget better</h2>
+            <h1 className="title">BUDGETNATOR</h1>
+            <h2 className="title-description">Insert description</h2>
             <button className="start-button">Get Started</button>   
+
+            <div className="input-container">
+                <label className="income-label">Yearly Income:</label>
+                <input
+                    type="number"
+                    value={income}
+                    onChange={e => setIncome(Number(e.target.value))}
+                />
+
+                <label className="amount-label">Amount:</label>
+                <input
+                    type="number"
+                    value={amount}
+                    onChange={e => setAmount(Number(e.target.value))}
+                />
+
+                <button className="submit-button" onClick={sendUserInput}>Submit</button>
+                {response && <p>{response}</p>}
+            </div>
         </div>
     );
 }

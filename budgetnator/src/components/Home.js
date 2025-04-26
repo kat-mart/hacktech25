950dc301895a7988758966b550d33fb6bd55bffe
Kelly Ung <kellyung@myyahoo.com>
2025-04-26 10:17:44 -0700

    add budgetnator folder

---
+++ budgetnator/src/components/Home.js
@@ -0,0 +1,11 @@
+import './Home.css';
+
+export default function Home() {
+    return (
+        <div>
+            <h1 className="title">BUGDETNATOR</h1>
+            <h2 className="title-description">Help you budget better</h2>
+            <button className="start-button">Get Started</button>   
+        </div>
+    );
+}

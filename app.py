cfa7d847949d78704ef4101cf48bd92027d01dba
Kelly Ung <kellyung@myyahoo.com>
2025-04-26 12:04:26 -0700

    update app.py

--- app.py
+++ app.py
@@ -8,23 +8,11 @@ app = Flask(__name__)
 CORS(app)  # allow all origins by default
 
 
-@app.route('/api/data', methods=['GET'])
-def get_data():
-   data = {"message": "Hello from Python!"}
-   return jsonify(data)
-
-
-@app.route('/api/user', methods=['GET'])
-def get_user():
-   user = {"name": "Alice", "age": 30}
-   return jsonify(user)
-
-
-@app.route('/api/message', methods=['POST'])
-def receive_message():
+@app.route('/api/user-input', methods=['POST'])
+def receive_user_input():
    data = request.get_json()
-   print("Received from frontend:", data)
-   return jsonify({"response": "Hi!"})
+   result = data['Income'] + 1000
+   return jsonify({"response": result})
 
 
 if __name__ == "__main__":

b2f28db1d7adddecd3c4ca4f00f2d0cb8b1150eb
Kelly Ung <kellyung@myyahoo.com>
2025-04-26 12:04:50 -0700

    update App.js

--- budgetnator/src/App.js
+++ budgetnator/src/App.js
@@ -1,36 +1,11 @@
 import './App.css';
-import React, { useState, useEffect } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import About from './components/About';
-import axios from 'axios';
 
 
 const App = () => {
-  const [data, setData] = useState(null);
-  const [response, setResponse] = useState('');
-
-  useEffect(() => {
-    axios.get('http://127.0.0.1:5000/api/user') // API endpoint from Flask
-      .then(response => {
-        setData(response.data);
-      })
-      .catch(error => {
-        console.error('There was an error!', error);
-      });
-  }, []);
-
-  const sendMessage = () => {
-    axios.post('http://127.0.0.1:5000/api/message', { message: "button clicked" })
-      .then(res => {
-        setResponse(res.data.response);
-      })
-      .catch(error => {
-        console.error('Error sending message:', error);
-      });
-  };
-
   return (
     <div className="App">
       <Navbar />
@@ -39,9 +14,6 @@ const App = () => {
         <Route path='/Home' element={<Home />} />
         <Route path='/About' element={<About />} />
       </Routes>
-      {/* {data ? <p>{data.name} {data.age}</p> : <p>Loading...</p>}
-      <button onClick={sendMessage}>Send Message</button>
-      {response && <p>{response}</p>} */}
     </div>
   );
 }
